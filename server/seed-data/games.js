72d5b0b1715e697c2c261da7849f8fd869e41806
Michelle-Watson <alicerockshard@gmail.com>
2024-11-28 16:04:56 -0500

    Add url for images to seed data

--- server/seed-data/games.js
+++ server/seed-data/games.js
@@ -2,43 +2,68 @@ export default [
   {
     id: 1,
     title: "Cyberpunk 2077",
-    description: "A futuristic RPG set in Night City.",
+    description:
+      "Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification.",
     release_date: "2020-12-10",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1091500/capsule_231x87.jpg?t=1732384908",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1091500/header.jpg?t=1732384908",
   },
   {
     id: 2,
     title: "The Witcher 3: Wild Hunt",
     description:
-      "An open-world RPG where you play as Geralt of Rivia, a monster hunter.",
+      "An open-world RPG. You are Geralt of Rivia, mercenary monster slayer. Before you stands a war-torn, monster-infested continent you can explore at will. Your current contract? Tracking down Ciri — the Child of Prophecy, a living weapon that can alter the shape of the world.",
     release_date: "2015-05-19",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/292030/capsule_231x87.jpg?t=1730212929",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/292030/header.jpg?t=1730212929",
   },
   {
     id: 3,
     title: "Red Dead Redemption 2",
     description:
-      "A Western-themed action-adventure game that follows Arthur Morgan's journey.",
+      "A Western-themed action-adventure game. Winner of over 175 Game of the Year Awards and recipient of over 250 perfect scores, RDR2 is the epic tale of outlaw Arthur Morgan and the infamous Van der Linde gang, on the run across America at the dawn of the modern age. Also includes access to the shared living world of Red Dead Online.",
     release_date: "2018-10-26",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1174180/capsule_231x87.jpg?t=1732026284",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1174180/header.jpg?t=1732026284",
   },
   {
     id: 4,
-    title: "The Elder Scrolls V: Skyrim",
+    title: "The Elder Scrolls V: Skyrim Special Edition",
     description:
-      "An open-world RPG where you explore the fantasy world of Skyrim.",
+      "Winner of more than 200 Game of the Year Awards, The Elder Scrolls V: Skyrim Special Edition brings the epic fantasy to life in stunning detail. The Special Edition includes the critically acclaimed game and add-ons with all-new features.",
     release_date: "2011-11-11",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/489830/capsule_231x87.jpg?t=1732565566",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/489830/header.jpg?t=1732565566",
   },
   {
     id: 5,
-    title: "Horizon Zero Dawn",
+    title: "Noita",
     description:
-      "An action RPG where you play as Aloy, battling robotic creatures in a post-apocalyptic world.",
+      "Noita is a magical action roguelite set in a world where every pixel is physically simulated. Fight, explore, melt, burn, freeze and evaporate your way through the procedurally generated world using spells you've created yourself.",
     release_date: "2017-02-28",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/881100/capsule_231x87.jpg?t=1732392763",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/881100/header.jpg?t=1732392763",
   },
   {
     id: 6,
-    title: "Minecraft",
+    title: "Hades",
     description:
-      "A sandbox game where players can build and explore pixelated worlds.",
-    release_date: "2011-11-18",
+      "A rogue-like dungeon crawler where you play as the son of Hades, fighting your way out of the underworld.",
+    release_date: "2020-09-17",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1145360/capsule_231x87.jpg?t=1730952074",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1145360/header.jpg?t=1730952074",
   },
   {
     id: 7,
@@ -46,61 +71,31 @@ export default [
     description:
       "An open-world action-adventure game set in the fictional city of Los Santos.",
     release_date: "2013-09-17",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/271590/capsule_231x87.jpg?t=1732792194",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/271590/header.jpg?t=1732792194",
   },
   {
     id: 8,
-    title: "The Legend of Zelda: Breath of the Wild",
+    title: "DAVE THE DIVER",
     description:
-      "A critically acclaimed action-adventure game in which you explore the open world of Hyrule.",
-    release_date: "2017-03-03",
+      "DAVE THE DIVER is a casual, singleplayer adventure RPG featuring deep-sea exploration and fishing during the day and sushi restaurant management at night. Join Dave and his quirky friends as they seek to uncover the secrets of the mysterious Blue Hole.",
+    release_date: "2023-06-28",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/271590/capsule_231x87.jpg?t=1732792194",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1868140/header.jpg?t=1732813395",
   },
   {
     id: 9,
-    title: "Overwatch",
+    title: "Stardew Valley",
     description:
-      "A team-based multiplayer first-person shooter featuring a diverse cast of heroes.",
-    release_date: "2016-05-24",
-  },
-  {
-    id: 10,
-    title: "Assassin's Creed Odyssey",
-    description:
-      "An action RPG set in Ancient Greece where you control a mercenary exploring the world.",
-    release_date: "2018-10-05",
-  },
-  {
-    id: 11,
-    title: "God of War (2018)",
-    description:
-      "A reboot of the series, following Kratos and his son Atreus in Norse mythology.",
-    release_date: "2018-04-20",
-  },
-  {
-    id: 12,
-    title: "Spider-Man (PS4)",
-    description:
-      "An open-world action game where you play as Spider-Man swinging through New York City.",
-    release_date: "2018-09-07",
-  },
-  {
-    id: 13,
-    title: "Dark Souls III",
-    description:
-      "A dark fantasy action RPG known for its difficulty and deep lore.",
-    release_date: "2016-04-12",
-  },
-  {
-    id: 14,
-    title: "Final Fantasy XV",
-    description:
-      "An action RPG following Noctis and his friends as they travel through the fantasy world of Eos.",
-    release_date: "2016-11-29",
-  },
-  {
-    id: 15,
-    title: "Animal Crossing: New Horizons",
-    description:
-      "A life-simulation game where players create their dream island on a deserted island.",
-    release_date: "2020-03-20",
+      "A farming simulation game where you inherit a farm and work to restore it to its former glory while interacting with the townspeople.",
+    release_date: "2016-02-26",
+    imageurlSmall:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/413150/capsule_231x87.jpg?t=1710874357",
+    imageurlBig:
+      "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/413150/header.jpg?t=1732769545",
   },
 ];
