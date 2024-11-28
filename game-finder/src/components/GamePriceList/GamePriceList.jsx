43e00f542d80e79b51484c6c2c6e7b89723aab44
Michelle-Watson <alicerockshard@gmail.com>
2024-11-27 21:25:21 -0500

    Rudimentary list of prices

--- game-finder/src/components/GamePriceList/GamePriceList.jsx
+++ game-finder/src/components/GamePriceList/GamePriceList.jsx
@@ -1,7 +1,78 @@
+import "./GamePriceList.scss";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export default function GamePriceList({ priceList, getPricesforGame }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredpriceList, setFilteredpriceList] = useState(priceList);
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filtered = priceList.filter((price) =>
+        [price.platform_name, price.discount, price.discounted_price]
+          .join(" ")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+      );
+      setFilteredpriceList(filtered);
+    } else {
+      setFilteredpriceList(priceList);
+    }
+  }, [priceList, searchTerm]);
+
+  const handleSearchChange = (event) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value);
+  };
+
+  const placeholderImageUrl =
+    "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/469600/capsule_231x87.jpg?t=1732210582"; // Placeholder image URL
+
   return (
-    <>
-      <h1>GamePriceList Component</h1>
-    </>
+    <div className="gameList">
+      <div className="header">
+        <h1 className="title">List of Prices</h1>
+        <div className="searchHeader">
+          <form className="searchHeader__form">
+            <input
+              type="text"
+              className="searchHeader__input"
+              placeholder="Search platforms..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </form>
+        </div>
+      </div>
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Platform</th>
+            <th>Discount</th>
+            <th>Discounted Price</th>
+            <th>Original Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredpriceList.map((price) => (
+            <tr key={price.id}>
+              <td>
+                <Link to={`/games/${price.id}`} className="gameList__link">
+                  <img
+                    src={placeholderImageUrl}
+                    alt={price.title}
+                    className="gameArt"
+                  />
+                </Link>
+              </td>
+              <td>{price.platform_name}</td>
+              <td>{price.discount}</td>
+              <td>{price.discounted_price}</td>
+              <td>{price.original_price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
