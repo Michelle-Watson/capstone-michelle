6c1a34c055575352923bb2fde90c480c590d2e86
Michelle-Watson <alicerockshard@gmail.com>
2024-11-28 16:23:43 -0500

    Have game details page show box art from db

--- game-finder/src/components/GameInfo/GameInfo.jsx
+++ game-finder/src/components/GameInfo/GameInfo.jsx
@@ -1,19 +1,13 @@
 import "./GameInfo.scss";
 
 export default function GameInfo({ currentGame }) {
-  const placeholderBIGImageUrl =
-    "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/469600/header.jpg?t=1732210582"; // Placeholder image URL
-
-  const { id, title, description, release_date } = currentGame;
+  const { id, title, description, release_date, imageurlSmall, imageurlBig } =
+    currentGame;
 
   return (
     <div className="gameInfo__container">
       <div className="gameInfo__imageContainer">
-        <img
-          src={placeholderBIGImageUrl}
-          alt={title}
-          className="gameInfo__image"
-        />
+        <img src={imageurlBig} alt={title} className="gameInfo__image" />
       </div>
       <div className="gameInfo__details">
         <h2>{title}</h2>
